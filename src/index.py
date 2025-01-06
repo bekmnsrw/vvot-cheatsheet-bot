26648f720f860c2b891338b40e8f3e5e9fbfd9ce
bekmnsrw <ilya.bekmansurov@gmail.com>
2025-01-06 22:19:54 +0300

    Minor changes

--- src/index.py
+++ src/index.py
@@ -9,7 +9,7 @@ def handler(event, context):
 
     if message:
         token = context.token["access_token"]
-        handle_message(message, token)
+        handle_message(message=message, token=token)
 
     return { 
         "statusCode": SUCCESS,
@@ -21,31 +21,31 @@ def handle_message(message, token):
 
     # Отправляем сообщение в ответ на команду /start
     if text == START_COMMAND:
-        send_message(START_MESSAGE, message)
+        send_message(text=START_MESSAGE, message=message)
 
     # Отправляем сообщение в ответ на команду /help
     elif text == HELP_COMMAND:
-        send_message(HELP_MESSAGE, message)
+        send_message(text=HELP_MESSAGE, message=message)
 
     # Если сообщение содержит обычный текст, обрабатываем его
     elif text is not None:
-        handle_text_message(text, message, token)
+        handle_text_message(text=text, message=message, token=token)
 
     # Если сообщение содержит изображение, обрабатываем его
     elif image := message.get("photo"):
-        handle_image_message(image, message, token)
+        handle_image_message(image=image, message=message, token=token)
 
     # В противном случае, если сообщение состоит не из текста или изображения, отправляем сообщение об ошибке
     else:
-        send_message(GET_UNHANDLED_MESSAGE_TYPE_ERROR_MESSAGE, message)
+        send_message(text=GET_UNHANDLED_MESSAGE_TYPE_ERROR_MESSAGE, message=message)
 
 def handle_text_message(text, message, token):
     gpt_answer = get_gpt_answer(question=text, token=token)
 
     if gpt_answer:
-        send_message(gpt_answer, message)
+        send_message(text=gpt_answer, message=message)
     else:
-        send_message(CANT_ANSWER_ERROR_MESSAGE, message)
+        send_message(text=CANT_ANSWER_ERROR_MESSAGE, message=message)
 
 def handle_image_message(image, message, token):
     image_id = image[-1]["file_id"]
@@ -54,6 +54,6 @@ def handle_image_message(image, message, token):
     recognized_text = get_recognized_text(base64_image=base64_image, token=token) 
 
     if recognized_text:
-        handle_text_message(recognized_text, message, token)
+        handle_text_message(text=recognized_text, message=message, token=token)
     else:
-        send_message(GET_INCORRECT_PHOTO_ERROR_MESSAGE, message)    
+        send_message(text=GET_INCORRECT_PHOTO_ERROR_MESSAGE, message=message)    
--- src/telegram_service.py
+++ src/telegram_service.py
@@ -24,19 +24,19 @@ def get_image_by_id(id):
     
     response = get(url=url, params=params)
 
-    if response.status_code == SUCCESS:
-        image_path = response.json()["result"].get("file_path")
-        get_image_by_path(image_path)
-    else:
+    if response.status_code != SUCCESS:
         return None
+    
+    image_path = response.json()["result"].get("file_path")
+    return get_image_by_path(image_path=image_path)
 
 def get_image_by_path(image_path):
     url = f"{TELEGRAM_IMAGE_URL}/{image_path}"
 
     response = get(url=url)
 
-    if response.status_code == SUCCESS:
-        image_bytes = response.content
-        return encode_to_base64(image_bytes)
-    else:
+    if response.status_code != SUCCESS:
         return None
+    
+    image_bytes = response.content
+    return encode_to_base64(bytes=image_bytes)
